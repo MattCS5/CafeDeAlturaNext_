4d78460a7920cfd93f3317007a6aa40f431e8581
MattCS5 <145207327+MattCS5@users.noreply.github.com>
2024-01-31 17:17:11 +0100

    pagina final

--- app/components/CheckOut.jsx
+++ app/components/CheckOut.jsx
@@ -1,28 +1,31 @@
 "use client";
-import React from 'react'
-import MetodoDePago from './MetodoDePago'
-import TotalCarrito from './TotalCarrito'
+import React from "react";
+import MetodoDePago from "./MetodoDePago";
+import TotalCarrito from "./TotalCarrito";
 import { useContext } from "react";
 import { DataContext } from "../context/DataContext";
 
 const CheckOut = () => {
   const { setOpen } = useContext(DataContext);
   return (
-    <div onClick={() => setOpen(false)} className='mt-28 pl-10 pr-10 pb-10 flex flex-col justify-center gap-6 '>
-        <div className='flex items-center justify-center'>
-            <h2 className='flex items-center text-[#2A5B45] font-medium text-2xl'>CheckOut</h2>
-        </div>
-       
-       <div className='flex gap-6 justify-around'>
-        <MetodoDePago/>
+    <div
+      onClick={() => setOpen(false)}
+      className="mt-28 pl-10 pr-10 pb-10 flex flex-col justify-center gap-6 "
+    >
+      <div className="flex items-center justify-center">
+        <h2 className="flex items-center text-[#2A5B45] font-medium text-2xl">
+          CheckOut
+        </h2>
+      </div>
+
+      <div className="flex gap-6 justify-around">
+        <MetodoDePago />
         <TotalCarrito checkout={true} />
-       </div>
+      </div>
 
-       <div className='flex gap-6' >
-        
-       </div>
+      <div className="flex gap-6"></div>
     </div>
-  )
-}
+  );
+};
 
-export default CheckOut
+export default CheckOut;
